341f77174e8a27d869d16831a3a40516c250eb84
Azkalyasin <yasinadzkiaazkal@gmail.com>
2024-03-08 10:58:10 +0700

    Add project files.

---
+++ praktikumAlgoritma/praktikumAlgoritma.cpp
@@ -0,0 +1,37 @@
+
+#include <iostream>
+using namespace std;
+
+int a[20];
+int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukkan banyak element pada array";
+		cin >> n;
+		if (n <= 20) {
+			break;
+		}
+		else
+		{
+
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
