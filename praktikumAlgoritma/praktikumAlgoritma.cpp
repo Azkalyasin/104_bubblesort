6ea306cfaa05c6b097938130403fe5fb4d3e545e
Azkalyasin <yasinadzkiaazkal@gmail.com>
2024-03-08 11:07:03 +0700

    Function Input

--- praktikumAlgoritma/praktikumAlgoritma.cpp
+++ praktikumAlgoritma/praktikumAlgoritma.cpp
@@ -15,10 +15,21 @@ void input() {
 		}
 		else
 		{
-
+			cout << "\nArray ddapat mempunyai maksimal 20 element/n";
 		}
 	}
+
+	cout << endl;
+	cout << "======================" << endl;
+	cout << "Masukkan element Array" << endl;
+	cout << "======================" << endl;
+
+	for (int i = 0; 1 < n; i++) {
+		cout << "Data ke" << (1 + 1) << ": ";
+		cin >> a[i];
+	}
 }
+
 
 
 
